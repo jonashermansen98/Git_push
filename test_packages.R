43ee6f5a784d54ad773fdd5419e53913d6143544
[jonashermansen98] <[202009355@post.au.dk]>
2023-12-17 19:32:49 +0000

    sådan

--- test_packages.R
+++ test_packages.R
@@ -1,13 +1,9 @@
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse) #hej matematik. 123
-=======
-library(tidyverse) #nnn
->>>>>>> 754a78e561e2665eab474c12e31f98e0e984b0a6
+library(tidyverse)
+library(tidyverse)
 
-<<<<<<< HEAD
-dfdf 
-try to push 
-=======
-Mærk lige den her 
->>>>>>> 8950b32f9cb1b81b2cc0d3827503815936ca19f1
+det kan jeg godt finde ud af.
+
+
+
+
